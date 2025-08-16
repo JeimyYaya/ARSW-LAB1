f65a7caf0dae859ebe659c489d23a49a973c3e7f
unknown <jeimy.yaya-m@labinfo.is.escuelaing.edu.co>
2025-08-16 09:37:13 -0500

    Primer commit

--- src/main/java/edu/eci/arsw/threads/CountThread.java
+++ src/main/java/edu/eci/arsw/threads/CountThread.java
@@ -9,6 +9,20 @@ package edu.eci.arsw.threads;
  *
  * @author hcadavid
  */
-public class CountThread {
-    
+public class CountThread implements Runnable{
+    private int A;
+    private int B;
+
+    public CountThread(int A, int B){
+        this.A = A;
+        this.B = B;
+
+    }
+
+    public void run(){
+        for (int i = A; i <= B; i++){
+            System.out.print(i + ", ");
+        }
+    }
 }
+    
--- src/main/java/edu/eci/arsw/threads/CountThreadsMain.java
+++ src/main/java/edu/eci/arsw/threads/CountThreadsMain.java
@@ -12,7 +12,13 @@ package edu.eci.arsw.threads;
 public class CountThreadsMain {
     
     public static void main(String a[]){
-        
+        Thread thread1 = new Thread(new CountThread(0,99));
+        Thread thread2 = new Thread(new CountThread(99,199));
+        Thread thread3 = new Thread(new CountThread(200, 299));
+
+        thread1.run();
+        thread2.run();
+        thread3.run();
     }
     
 }
